d9be9568450bbb7f23e15940aae13b663f701aef
ttran1272 <tu_anh11@hotmail.com>
2017-09-22 16:34:31 -0700

    some updated

---
+++ app/src/main/java/edu/orangecoastcollege/cs273/ttran1272/occars2/LoanSummaryActivity.java
@@ -0,0 +1,13 @@
+package edu.orangecoastcollege.cs273.ttran1272.occars2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoanSummaryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loan_summary);
+    }
+}
